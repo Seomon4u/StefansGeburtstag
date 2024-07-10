8d8f8ce1223f167a9e6f7ea30f6e7eb7266e68b7
StefanNeubauer <e0926175@student.tuwien.ac.at>
2024-07-10 23:36:16 +0200

    background fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import styles from "./layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className={styles.html} lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
